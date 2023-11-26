56e70439263a9062c2281e9006a553c70da9ecc7
Arnaud Gathy <arnaud.gathy@gmail.com>
2023-11-26 11:45:16 +0100

    fix eslint error

--- src/components/MyList.js
+++ src/components/MyList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Form, Field } from 'react-final-form'
 import { dbRefs, useDatabase } from '../hooks/useDatabase'
 import styled from 'styled-components'
